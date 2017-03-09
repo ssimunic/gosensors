d13e8c930be0016a7bfbef266be5b66642251a41
Silvio Simunic <silvio.simunic@gmail.com>
2017-03-09 17:47:27 +0100

    Init

---
+++ README.md
@@ -0,0 +1,53 @@
+# gosensors
+
+Hardware sensors using Go and lm-sensors
+
+## Setup
+* Install `lm-sensors` with `sudo apt get install lm-sensors`
+* `go get github.com/ssimunic/gosensors` in your working directory
+
+## Example
+
+### Program
+
+```go
+package main
+
+import (
+	"fmt"
+	"github.com/ssimunic/gosensors"
+)
+
+func main() {
+	sensors, err := gosensors.NewFromSystem()
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Sensors implements Stringer interface,
+	// so code below will print out JSON
+	fmt.Println(sensors)
+
+	// Also valid
+	// fmt.Println("JSON:", sensors.JSON())
+
+	// Iterate over chips
+	for chip := range sensors.Chips {
+		// Iterate over entries
+		for key, value := range sensors.Chips[chip] {
+			// If CPU or GPU, print out
+			if key == "CPU" || key == "GPU" {
+				fmt.Println(key, value)
+			}
+		}
+	}
+}
+```
+
+### Output:
+```
+{"chips":{"acpitz-virtual-0":{"Adapter":"Virtual device","temp1":"+25.0°C  (crit = +107.0°C)"},"coretemp-isa-0000":{"Adapter":"ISA adapter","Core 0":"+76.0°C  (high = +105.0°C, crit = +105.0°C)","Core 1":"+74.0°C  (high = +105.0°C, crit = +105.0°C)","Physical id 0":"+76.0°C  (high = +105.0°C, crit = +105.0°C)"},"dell_smm-virtual-0":{"Adapter":"Virtual device","CPU":"+65.0°C","GPU":"+50.0°C","Other":"+50.0°C","Processor Fan":"2200 RPM","SODIMM":"+39.0°C","fan2":"2200 RPM"}}}
+CPU +65.0°C
+GPU +50.0°C
+```
---
+++ sensors.go
@@ -0,0 +1,93 @@
+package gosensors
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"os/exec"
+	"strings"
+)
+
+// Sensors struct represents lm-sensors output.
+// Content field contains string output.
+// Chips field contains map[string]Entries.
+// Example (JSON style):
+// {
+// 		"coretemp-isa-0000": {
+//			"CPU": "+60.0°C",
+//			"GPU": "+48.0°C",
+// 		}
+// }
+type Sensors struct {
+	Content string             `json:"-"`
+	Chips   map[string]Entries `json:"chips"`
+}
+
+// Entries representing key, value pairs for chips.
+// Example (JSON style):
+// {
+// 		"GPU": "+56.0°C"
+// 		"CPU": "+68.0°C"
+// }
+type Entries map[string]string
+
+func construct(content string) *Sensors {
+	s := &Sensors{}
+	s.Content = content
+	s.Chips = map[string]Entries{}
+
+	lines := strings.Split(s.Content, "\n")
+
+	var chip string
+	for _, line := range lines {
+		if len(line) > 0 {
+			if !strings.Contains(line, ":") {
+				chip = line
+				s.Chips[chip] = Entries{}
+			} else if len(chip) > 0 {
+				parts := strings.Split(line, ":")
+				entry := parts[0]
+				value := strings.TrimRight(strings.TrimLeft(parts[1], " "), " ")
+				s.Chips[chip][entry] = value
+			}
+		}
+	}
+
+	return s
+}
+
+// NewFromSystem executes "sensors" system command and returns constructed Sensors struct.
+// A successful call returns err == nil.
+func NewFromSystem() (*Sensors, error) {
+	out, err := exec.Command("sensors").Output()
+	if err != nil {
+		return &Sensors{}, errors.New("lm-sensors missing")
+	}
+
+	s := construct(string(out))
+
+	return s, nil
+}
+
+// NewFromFile reads content from log file and returns constructed Sensors struct.
+// A successful call returns err == nil.
+func NewFromFile(path string) (*Sensors, error) {
+	out, err := ioutil.ReadFile(path)
+	if err != nil {
+		return &Sensors{}, err
+	}
+
+	s := construct(string(out))
+	return s, nil
+}
+
+// JSON returns JSON of Sensors.
+func (s *Sensors) JSON() string {
+	out, _ := json.Marshal(s)
+
+	return string(out)
+}
+
+func (s *Sensors) String() string {
+	return s.JSON()
+}
